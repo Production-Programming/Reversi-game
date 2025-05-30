27c771e178e5793a9bd7a6d1ed2beb151e8b9cc3
TayaKlimakova <96236885+TayaKlimakova@users.noreply.github.com>
2025-05-31 00:05:22 +0300

    Adding script to menu.js

--- public/assets/js/menu.js
+++ public/assets/js/menu.js
@@ -1,3 +1,22 @@
 $( () => {
     $('#userInfo').attr("href", "user.html?username=" + username);
 });
+
+fetch('https://dummyjson.com/quotes/random')
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('quoteBox').innerHTML = 
+        `<span style="font-weight: bold; font-style: normal;">Твоя цитата на сегодня: </span>"${data.quote}" — ${data.author}`;
+    })
+    .catch(error => {
+        console.error('Ошибка при загрузке цитаты:', error);
+    });
+
+fetch('https://ipapi.co/json/')
+    .then(response => response.json())
+    .then(location => {
+    const flagEmoji = location.country_code
+        ? String.fromCodePoint(...[...location.country_code].map(c => 127397 + c.charCodeAt()))
+        : '';
+    document.getElementById('countryInfo').textContent = `Привет из ${location.city}, ${location.country_name} ${flagEmoji}`;
+    });
